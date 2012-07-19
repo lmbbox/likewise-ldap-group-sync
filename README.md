7ddc532103e400f5beb45be6327cd94332f251ff
Thomas Montague <montague.thomas@lmbbox.com>
2012-07-19 12:14:22 -0400

    Initial import of ldap group sync script.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-likewise-ldap-group-sync
-========================
+# Likewise LDAP Group Sync
+
+This is a script that will work with Likewise's LDAP integration to find the user accounts in a given LDAP group and add them to a local group in linux.
+
+## Usage
+
+First you need to create configuration files in the group.d folder. Use the .example file as a reference when creating yours (see below).
+
+	# LDAP Group Membership Sync Config
+	ADGROUP=Linux^Admins
+	LOCALGROUP=admin
+	DEFAULTUSERS=usera,userb
+
+You must set all three values, where:
+
+	ADGROUP is the LDAP group you which to add users from
+	LOCALGROUP is the local linux group you wish to add the ADGROUP users to
+	DEFAULTUSERS is a way to make sure local users stay on the group you are updating via LDAP
---
+++ group.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+echo "LDAP Group Membership Sync Script"
+
+
+# Check that this distribution is Ubuntu
+if grep -qvi ubuntu <<< `uname -v`
+then
+	echo "This script is meant for Ubuntu distributions only."
+	exit 1
+fi
+
+
+# Check if root
+if [ $UID != 0 ]
+then
+	echo "You are not root. This script must be run with root permissions."
+	exit 1
+fi
+
+
+# Filepath
+root=$(dirname $(readlink -f $0))
+
+
+# Read config file and assign values
+for file in $root/group.d*
+do
+	source $file
+	echo $file
+	echo $ADGROUP
+	echo $LOCALGROUP
+	echo $DEFAULTUSERS
+	
+	USERS=`lw-enum-members $ADGROUP | awk -vORS=',' '/SAM account name:/ {print $4}' | sed '$s/.$//'`
+	
+	if [ -n $DEFAULTUSERS ]
+	then
+		if [ -n $USERS ]
+		then
+			USERS="$DEFAULTUSERS,$USERS"
+		else
+			USERS="$DEFAULTUSERS"
+		fi
+	fi
+	
+	echo $USERS
+	
+	sudo sed -i 's/^\('$LOCALGROUP':[^:]*:[0-9][0-9]*:\).*$/\1'$USERS'/' /etc/group
+done
+
+echo
+echo "LDAP Group Membership Sync Complete."
